bf41a4c1dcd63adc9ac8fc53d3dfbebf355a486a
Fitrah-16520207 <73836810+Fitrah-16520207@users.noreply.github.com>
2020-11-02 21:36:20 +0700

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+   SECRET_KEY ='b1547ea2b7e1492bcc3f3bc2e0e52411'
+   SQLALCHEMY_DATABASE_URI ='sqlite:///site.db'
+   MAIL_SERVER = 'stmp.gmail.com'
+   MAIL_PORT = 587
+   MAIL_USE_TLS = True
+   MAIL_USERNAME = 'fitnug30@gmail.com'
+   MAIL_PASSWORD = '3o-ii-2oo1'
